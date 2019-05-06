0e7d8152a5c32ef4e99fe3ca32430a17aa6a6306
omedale <omedale@gmail.com>
2019-05-06 23:47:56 +0100

    add search

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'search/index'
   devise_for :users
   get 'home/index'
 
   root to: "home#index"
-
+  get 'search' => 'search#index'
   resources :posts, only: [:new, :create,  :show, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
