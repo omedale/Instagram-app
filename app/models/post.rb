82ce71eb698900ba89121b31cbc141dcba53a4ee
omedale <omedale@gmail.com>
2019-05-06 23:23:32 +0100

    - create HashTag model - create pivot table between Post and HashTag -  add many-to-many relationship between POST and HashTag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_hash_tags
+  has_many :hash_tags, through: :post_hash_tags
 
   has_one_attached :image
   validate :image_presence
